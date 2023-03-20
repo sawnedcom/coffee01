faf90acd0c3ff720ac738d6805e36d29ca96004c
sawnedcom <101937469+sawnedcom@users.noreply.github.com>
2023-03-20 17:44:10 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+function astronotCof() {
+    let astro = document.getElementById("astro")
+    let regular = document.getElementById("regular")
+    let asli = document.getElementById("asli")
+    regular.style.display = "none"
+    astro.style.display = "block"
+    asli.style.display = "none"
+}
+
+function regularCof () {
+    let astro = document.getElementById("astro")
+    let regular = document.getElementById("regular")
+    let asli = document.getElementById("asli")
+    regular.style.display = "block"
+    astro.style.display = "none"
+    asli.style.display = "none"
+}
+
+function asliCof() {
+    let astro = document.getElementById("astro")
+    let regular = document.getElementById("regular")
+    let asli = document.getElementById("asli")
+    regular.style.display = "none"
+    astro.style.display = "none"
+    asli.style.display = "block"
+}
+
+function maaf() {
+    let maff = document.getElementById("maff")
+    let maff2 = document.getElementById("maff2")
+    let maff3 = document.getElementById("maff3")
+    maff.style.display = "block"
+    maff2.style.display = "block"
+    maff3.style.display = "block"
+}
